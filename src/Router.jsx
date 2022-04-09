465a32cff83326ee3b6c817c6195b9231e0e8e55
Xharles <charlesnzubechukwu000@gmail.com>
2022-04-09 08:17:10 +0100

    rink to rop

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,23 +3,20 @@ import Home from "./pages/HomePage";
 import MintPage from "./pages/MintPage";
 import DaosPage from "./pages/DaosPage";
 
-
 function Router() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/daos" element={<DaosPage />} />
-        <Route path="/mint" element={<MintPage />} />
-        <Route
-          path="/:slug/proposals"
-          element={<DaosPage type="proposals" />}
-        />
-        <Route path="/:slug/about" element={<DaosPage type="about" />} />
-        <Route path="/:slug/proposals/new" element={<DaosPage type="new" />} />
-        <Route path="/:slug/proposal/:id" element={<DaosPage type="proposal" />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/daos" element={<DaosPage />} />
+      <Route path="/mint" element={<MintPage />} />
+      <Route path="/:slug/proposals" element={<DaosPage type="proposals" />} />
+      <Route path="/:slug/about" element={<DaosPage type="about" />} />
+      <Route path="/:slug/proposals/new" element={<DaosPage type="new" />} />
+      <Route
+        path="/:slug/proposal/:id"
+        element={<DaosPage type="proposal" />}
+      />
+    </Routes>
   );
 }
 
