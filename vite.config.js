79ecd8b40f2653ce855319f5dcc684aa960268e0
Xharles <charlesnzubechukwu000@gmail.com>
2022-06-16 15:52:16 +0100

    dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from '@honkhonk/vite-plugin-svgr'
+import svgr from "@honkhonk/vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
