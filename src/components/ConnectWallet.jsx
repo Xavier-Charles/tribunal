37678a3d6e5dcab55843a58ff74bc5cfdaf17224
Xharles <charlesnzubechukwu000@gmail.com>
2022-09-15 14:01:06 +0100

    ui fixes

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -13,25 +13,25 @@ const ConnectWallet = ({
       <div className="container max-w-xl py-12 mx-auto">
         <div className="w-full flex flex-col sm:flex-row items-center mx-auto">
           <h3 className="flex-grow text-center text-base title-font text-gray-600 mb-5">
-            Connect Wallet to filter DAOs by your NFTs.
+            Connect your wallet to filter DAOs by your NFTs.
           </h3>
         </div>
         <div className="flex w-full justify-center">
           <button
             onClick={handleCBAuthenticate}
-            className="flex-shrink-0 flex text-cadet border-blue-800 border py-2 px-4 focus:outline-none rounded text-lg mt-10 sm:mt-0 mr-2"
+            className="flex-shrink-0 flex text-cadet hover:bg-blue-700 hover:bg-opacity-5 border-blue-700 border py-2 px-4 focus:outline-none rounded text-lg mt-10 sm:mt-0 mr-2"
           >
             <Coinbase className="w-6" />
           </button>
           <button
             onClick={handleAuthenticate}
-            className="flex-shrink-0 flex text-cadet border-gold border py-2 px-4 focus:outline-none rounded text-lg mt-10 sm:mt-0 mx-2"
+            className="flex-shrink-0 flex text-cadet hover:bg-gold hover:bg-opacity-5 border-gold border py-2 px-4 focus:outline-none rounded text-lg mt-10 sm:mt-0 mx-2"
           >
             <Metamask className="w-[22px] self-center" />
           </button>
           <button
             onClick={handleUAuthenticate}
-            className="flex-shrink-0 flex text-cadet border-blue-800 border py-2 px-4 focus:outline-none rounded text-lg mt-10 sm:mt-0 ml-2"
+            className="flex-shrink-0 flex text-cadet hover:bg-blue-600 hover:bg-opacity-5 border-blue-600 border py-2 px-4 focus:outline-none rounded text-lg mt-10 sm:mt-0 ml-2"
           >
             <Unstoppable className="w-6" />
           </button>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = ({
   return (
     <section className="text-gray-600 body-font pt-28 lg:pt-24">
       <div className="container max-w-7xl px-5 py-24 mx-auto">
-        <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+        <div className="flex flex-wrap w-full mb-16 sm:mb-20 flex-col items-center text-center">
           <h1 className="sm:text-3xl text-2xl font-serif font-medium mb-2 text-gray-900">
             Tribunals
           </h1>
